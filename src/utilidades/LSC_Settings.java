c99c8e6fb211f9d679d35c4007a55957074c3b07
Lenovo <Lenovo@Lenovo-PC>
2014-11-25 10:03:30 -0200

    Classe e imagens para os testes de Settings

---
+++ src/utilidades/LSC_Settings.java
@@ -0,0 +1,58 @@
+package utilidades;
+
+public class LSC_Settings extends LSC_Main {
+
+	public String anounimous_usage_settings;
+	public String backup_settings;
+	public String battery_settings;
+	public String btn_Settings;
+	public String cancel_settings;
+	public String deviceManager_settings;
+	public String enableAlerts_settings;
+	public String firewall_settings;
+	public String hardwareScan_settings;
+	public String LSC_Windows_notification;
+	public String memory_settings;
+	public String productRegistration_settings;
+	public String recoveryMedia_settings;
+	public String save_settings;
+	public String softwareUpdates_settings;
+	public String storageDeveices_settings;
+	public String virusProtection_settings;
+	public String warranty_settings;
+	public String welcomeScreen_settings;
+	
+	
+	public String enable_alert_checked;
+	public String enable_alert_unchecked;
+	
+	public LSC_Settings(String idioma) {
+		super(idioma);
+		anounimous_usage_settings = "imgs/"+idioma+"/settings/anonimous_usage_settings.PNG";
+		backup_settings = "imgs/"+idioma+"/settings/backup_settings.PNG";
+		battery_settings = "imgs/"+idioma+"/settings/battery_settings.PNG";
+		btn_Settings = "imgs/"+idioma+"/settings/btn_settings.PNG";
+		cancel_settings = "imgs/"+idioma+"/settings/cancel_settings.PNG";
+		deviceManager_settings = "imgs/"+idioma+"/settings/deviceManager_settings.PNG";
+		enableAlerts_settings = "imgs/"+idioma+"/settings/enableAlerts_settings.PNG";;
+		firewall_settings = "imgs/"+idioma+"/settings/firewall_settings.PNG";
+		hardwareScan_settings = "imgs/"+idioma+"/settings/hardwareScan_settings.PNG";
+		LSC_Windows_notification = "imgs/"+idioma+"/settings/LSC_Windows_notification.PNG";
+		memory_settings = "imgs/"+idioma+"/settings/memory_settings.PNG";
+		productRegistration_settings = "imgs/"+idioma+"/settings/productRegistration_settings.PNG";
+		recoveryMedia_settings= "imgs/"+idioma+"/settings/recoveryMedia_settings.PNG";
+		save_settings = "imgs/"+idioma+"/settings/save_settings.PNG";
+		softwareUpdates_settings = "imgs/"+idioma+"/settings/softwareUpdates_settings.PNG";
+		storageDeveices_settings = "imgs/"+idioma+"/settings/storageDevices_settings.PNG";
+		virusProtection_settings = "imgs/"+idioma+"/settings/virusProtection_settings.PNG";
+		warranty_settings = "imgs/"+idioma+"/settings/warranty_settings.PNG";
+		welcomeScreen_settings = "imgs/"+idioma+"/settings/welcomeScreen_settings.PNG";
+		
+		enable_alert_checked = "imgs/"+idioma+"/settings/enable_alert_checked.PNG";
+		enable_alert_unchecked = "imgs/"+idioma+"/settings/enabled_alert_unchecked.PNG";
+		
+	}
+	
+	
+	
+}
