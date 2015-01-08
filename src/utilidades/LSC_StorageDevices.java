9f51b4abc9ca8dadc3de2a1a4bfbe6293ec5eafc
aristeuazevedo <aristeu.azevedo@gmail.com>
2015-01-08 16:36:31 -0200

    Device Manager images added

--- src/utilidades/LSC_StorageDevices.java
+++ src/utilidades/LSC_StorageDevices.java
@@ -1,5 +1,17 @@
 package utilidades;
 
-public class LSC_StorageDevices {
+public class LSC_StorageDevices extends LSC_Main {
 
+	public String btnDiskCleanup = "imgs/"+idioma+"/storageDevices/btnDiskCleanup.PNG";
+	public String btnOptimize = "imgs/"+idioma+"/storageDevices/btnOptimize.PNG";
+	public String btnShopnow = "imgs/"+idioma+"/storageDevices/btnShopnow.PNG";
+	public String GetMoreStorage = "imgs/"+idioma+"/storageDevices/GetMoreStorage.PNG";
+	public String Lenovo_tips = "imgs/"+idioma+"/storageDevices/Lenovo_tips.PNG";
+	public String Optimize_performance = "imgs/"+idioma+"/storageDevices/Optimize_performance.PNG";
+	public String Optimize_space = "imgs/"+idioma+"/storageDevices/Optimize_space.PNG";
+
+	public LSC_StorageDevices(String idioma) {
+		super(idioma);
+	}
+	
 }
