2d2ed38e40c43c6c1ca1c21e7ee8b3e229b286d4
aristeuazevedo <aristeu.azevedo@gmail.com>
2015-01-19 16:46:01 -0200

    TestSelection Removed

--- src/gui/TestSelect.java
+++ src/gui/TestSelect.java
@@ -427,7 +427,7 @@ public class TestSelect extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TestSelection().setVisible(true);
+               // new TestSelection().setVisible(true);
             }
         });
     }
