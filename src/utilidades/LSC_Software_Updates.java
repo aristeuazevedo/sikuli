b75c49751b695126ab99bd3db9e830241ecf8bc4
Carlos Cardia <carlos.cardia@fit-tecnologia.org.br>
2015-01-30 10:39:12 -0200

    Software Updates Fixes

--- src/utilidades/LSC_Software_Updates.java
+++ src/utilidades/LSC_Software_Updates.java
@@ -7,6 +7,9 @@ public class LSC_Software_Updates extends LSC_Main {
 	public String SU_Screen_Descrip = "imgs/"+idioma+"/Software_Updates_Figures/Software_Updates_Screen_Description.PNG";
 	public String SU_Screen_Title = "imgs/"+idioma+"/Software_Updates_Figures/Software_Updates_Screen_Title.PNG";
 	public String SU_No_Lenovo_Software = "imgs/"+idioma+"/Software_Updates_Figures/SU_No_Lenovo_Software.PNG";
+	public String SU_Lenovo_Download = "imgs/"+idioma+"/Software_Updates_Figures/Software_Updates_Download_Button.PNG";
+	public String SU_Win_Update_Green = "imgs/"+idioma+"/Software_Updates_Figures/SU_Win_Update_Green.PNG";
+	public String SU_Win_Update_Launch = "imgs/"+idioma+"/Software_Updates_Figures/SU_Win_Update_Launch.PNG";
 	
 	
 	
