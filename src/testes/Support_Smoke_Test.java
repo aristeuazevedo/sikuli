04957a93f64c96555ff7b7f71396c0b31e1de8b0
Lenovo <Lenovo@Lenovo-PC>
2015-01-31 22:50:54 -0200

    refatoração da classe para armazenar resultados dos testes

--- src/testes/Support_Smoke_Test.java
+++ src/testes/Support_Smoke_Test.java
@@ -4,6 +4,8 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.sikuli.api.DesktopScreenRegion;
 import org.sikuli.api.ImageTarget;
@@ -17,63 +19,47 @@ import utilidades.LSC_Security;
 import utilidades.LSC_Support;
 import utilidades.LSC_System;
 import utilidades.ResultExec;
+import utilidades.Utilidades;
 
 public class Support_Smoke_Test {
 	String idioma = "";
 	
 	LSC_Support sys;
-	
-	ResultExec resultado = new ResultExec(" Test Support");
+	private List<ResultExec> listaResultados = new ArrayList<ResultExec>();
+	ResultExec resultado = new ResultExec("Smoke Test Support");
 	
 	Screen s = new Screen();
+	
 	public Support_Smoke_Test( String idioma){
 		
 		this.idioma = idioma;
 		sys = new LSC_Support(idioma);
 	}
 	
-	public void Smoke() {
-		try {
-			java.awt.Desktop.getDesktop().open(new File("Linguagens/"+idioma+".bat"));
+	public List<ResultExec> Smoke() {
+	
+			listaResultados.add(resultado);
 			
-			System.out.print(sys.LSC_Title);
+			titleLSC();
 			
-			s.wait(sys.LSC_Title,30.0);
-			s.click(sys.LSC_Title);
+			tabUnselected();
 			
-			s.wait(2.0);
-			s.click(sys.SupportUns);
-			s.wait(2.0);
 			VerificaTela(sys.onlinesupportIcon);
 			
-			s.wait(2.0);
-			s.click(sys.SupportSel);
-			s.wait(2.0);
+			tabSelected();
 			VerificaTela(sys.supportModule_WarrantyIcon);
 						
-			s.wait(2.0);
-			s.click(sys.SupportSel);
-			s.wait(2.0);
+			tabSelected();
 			VerificaTela(sys.icn_configHistory);
 			
-			
-								
-			} catch (IOException e) {
-			
-			System.out.println("Imagem não encontrada");
-			e.printStackTrace();
-			}catch (Exception e) {
-			
-			e.printStackTrace();
-			resultado.addMensagens(e.toString());
-			}				
-		
+				
+		return listaResultados;
 	}
 
-	
 	public void VerificaTela(String icone)
 	{
 		try {
+			resultado = new ResultExec(Utilidades.nomeIcone(icone));
 			s.click(sys.LSC_Title);
 			s.wait(icone,10.0);
 			s.click(icone);
@@ -114,13 +100,59 @@ public class Support_Smoke_Test {
 				s.wait(icone);
 			}
 			
-			
+			resultado.addMensagens("Passed");
 			
 				
 		} catch (Exception e) {
 			e.printStackTrace();
 			resultado.addMensagens(e.toString());
 		}
-		
+		listaResultados.add(resultado);
 	}
+
+	//TODO Verificação do Titulo do LSC
+	public void titleLSC(){
+			
+				try {
+					resultado = new ResultExec("LSC Title verification");		
+					s.wait(sys.LSC_Title,30.0);
+					s.click(sys.LSC_Title);
+					resultado.addMensagens("Passed");
+				} catch (FindFailed e) {
+					e.printStackTrace();
+					resultado.addMensagens(e.toString());
+				}
+				listaResultados.add(resultado);
+		}
+		
+	//TODO Verifica a aba System não selecionada
+	public void tabUnselected(){
+					try {
+						resultado = new ResultExec("Tab Support Unselected");
+						s.wait(2.0);
+						s.click(sys.SupportUns);
+						s.wait(2.0);
+						resultado.addMensagens("Passed");
+					} catch (FindFailed e) {
+						resultado.addMensagens(e.toString());
+						e.printStackTrace();
+					}
+					listaResultados.add(resultado);
+		}
+		
+	public void tabSelected(){
+			try {
+				resultado = new ResultExec("Tab Support Selected");
+				s.wait(2.0);
+				s.click(sys.SupportSel);
+				s.wait(2.0);
+				resultado.addMensagens("Passed");
+			} catch (FindFailed e) {
+				resultado.addMensagens(e.toString());
+				e.printStackTrace();
+			}
+			listaResultados.add(resultado);
+		}
+	
+
 }
