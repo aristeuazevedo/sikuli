bf7d36cccda9f850eac378bd213ee052b9ea66fc
aristeuazevedo <aristeu.azevedo@gmail.com>
2015-02-02 17:04:11 -0200

    Machine type added (To specify which machine will be tested)

---
+++ src/utilidades/Machine_Type.java
@@ -0,0 +1,23 @@
+package utilidades;
+
+public class Machine_Type {
+
+	public String Windows;
+	
+	public boolean alert_test;
+	public boolean backup_test;
+	public boolean dashboard_test;
+	public boolean device_test;
+	public boolean hardware_test;
+	public boolean memory_test;
+	public boolean settings_test;
+	public boolean softwareupdates_test;
+	public boolean storagedevices_test;
+	
+	public boolean system_test;
+	public boolean security_smoke;
+	public boolean checkup_smoke;
+	public boolean support_test;
+	
+	
+}
